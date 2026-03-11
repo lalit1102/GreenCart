4206c3961c2559309cbd012c536fbdad491501f5
Lalit Baldaniya <lalit.baldaniya92@gmail.com>
2026-03-11 09:11:10 +0530

    cmit

--- server/models/product.js
+++ server/models/product.js
@@ -1,19 +1,17 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    description: { type: Array, required: true },
-    price: { type: Number, required: true },
-    offerPrice: { type: Number, required: true },
-    image: { type: Array, required: true },
-    category: { type: Array, required: true },
-    inStock: { type: Boolean, default: true },
-  },
-  { timestamps: true },
-);
 
-const product =
-  mongoose.models.product || mongoose.model("product", productSchema);
+const productSchema = new mongoose.Schema({
+  name:{type:String,required:true},
+  description:{type:Array,required:true},
+  price:{type:Number,required:true},
+  offerPrice:{type:Number,required:true},
+  image:{type:Array,required:true},
+  category:{type:Array,required:true},
+  inStock:{type:Boolean,default:true}
 
-export default product;
+},{ timestamps:true})
+
+const product = mongoose.models.product || mongoose.model("product",productSchema)
+
+export default product
